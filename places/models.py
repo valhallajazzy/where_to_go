10973e9c580d5856e8f467c35e4e9b3b26c877f7
valhallajazzy <valhallajazzy@mail.ru>
2023-10-20 15:19:04 +0300

    Add 'place' and 'Image' models. Add setup media files serving

--- places/models.py
+++ places/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coordinates = models.JSONField()
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    number = models.PositiveIntegerField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.number) + ' ' + str(self.place)
